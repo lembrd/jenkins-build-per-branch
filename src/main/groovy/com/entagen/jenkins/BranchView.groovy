0890647f6ce1500dcc4cc8fbba6e4d7b5816ba9f
lembrand <lembrd@gmail.com>
2015-05-17 03:47:38 +0300

    up

--- src/main/groovy/com/entagen/jenkins/BranchView.groovy
+++ src/main/groovy/com/entagen/jenkins/BranchView.groovy
@@ -4,8 +4,12 @@ class BranchView {
     String templateJobPrefix
     String branchName
 
+    public String getSafePrefix() {
+        return templateJobPrefix.replaceAll('.','').replaceAll('*','')
+    }
+
     public String getViewName() {
-        return "$templateJobPrefix-$safeBranchName"
+        return "$safePrefix-$safeBranchName"
     }
 
     public String getSafeBranchName() {
